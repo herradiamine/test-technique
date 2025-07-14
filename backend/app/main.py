568e646249e06d0a32d3155b25d67a3879433fba
Amine HERRADI <amineherradi@gmail.com>
2025-07-14 12:01:43 +0200

    16. Tests automatisés

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,7 +52,7 @@ app.add_middleware(
 )
 
 # Crée les tables si elles n'existent pas
-# models.Base.metadata.create_all(bind=database.engine)
+models.Base.metadata.create_all(bind=database.engine)
 
 app.include_router(router)
 
